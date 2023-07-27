6c923b91db43e38860a18d811adfe942413294c4
Dónal Murray <donalm@seadanda.dev>
2023-07-27 08:57:27 +0100

    Add shell sort to benchmarks

--- benches/sorting_benchmark.rs
+++ benches/sorting_benchmark.rs
@@ -1,6 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use sorting::{bubble_sort, insertion_sort, selection_sort};
+use sorting::{bubble_sort, insertion_sort, selection_sort, shell_sort};
 
 fn random_benchmark(c: &mut Criterion) {
     let test_data: Vec<u32> = black_box(vec![2, 5, 1, 6, 3, 4]);
@@ -14,6 +14,8 @@ fn random_benchmark(c: &mut Criterion) {
     });
 
     c.bench_function("bubble sort", |b| b.iter(|| bubble_sort(test_data.clone())));
+
+    c.bench_function("shell sort", |b| b.iter(|| shell_sort(test_data.clone())));
 }
 
 fn sorted_benchmark(c: &mut Criterion) {
@@ -28,6 +30,8 @@ fn sorted_benchmark(c: &mut Criterion) {
     });
 
     c.bench_function("bubble sort", |b| b.iter(|| bubble_sort(test_data.clone())));
+
+    c.bench_function("shell sort", |b| b.iter(|| shell_sort(test_data.clone())));
 }
 
 fn reversed_benchmark(c: &mut Criterion) {
@@ -42,6 +46,8 @@ fn reversed_benchmark(c: &mut Criterion) {
     });
 
     c.bench_function("bubble sort", |b| b.iter(|| bubble_sort(test_data.clone())));
+
+    c.bench_function("shell sort", |b| b.iter(|| shell_sort(test_data.clone())));
 }
 
 fn few_unique_benchmark(c: &mut Criterion) {
@@ -56,6 +62,8 @@ fn few_unique_benchmark(c: &mut Criterion) {
     });
 
     c.bench_function("bubble sort", |b| b.iter(|| bubble_sort(test_data.clone())));
+
+    c.bench_function("shell sort", |b| b.iter(|| shell_sort(test_data.clone())));
 }
 
 criterion_group!(
